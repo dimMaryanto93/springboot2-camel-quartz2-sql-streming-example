099dbbd2a494e34c6e4593bc6a8eec47daeb6fd3
dimasm93 <software.dimas_m@icloud.com>
2019-11-21 19:51:25 +0700

    refactore code

--- migration/sqlserver/V2019_11_05_19_54_49__base-tabel.sql
+++ migration/sqlserver/V2019_11_05_19_54_49__base-tabel.sql
@@ -6,13 +6,3 @@ create table Vehicles
     PoliceNumber varchar(100),
     CreatedDate  datetime default current_timestamp
 );
-
-create table VehiclesTemp
-(
-    Id           varchar(64)  not null primary key,
-    VehicleId    varchar(64),
-    CustomerName varchar(100) not null,
-    VinCode      varchar(100) not null,
-    PoliceNumber varchar(100),
-    CreatedDate  datetime default current_timestamp
-);
